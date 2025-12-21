96ecb3f3304b30097d3e7ae83da531c04343363e
Viv <averse.abfun@gmail.com>
2025-12-21 11:35:41 -0600

    session 2b: variable time-stretch (glide) works

--- src/dsp/stretch.rs
+++ src/dsp/stretch.rs
@@ -5,7 +5,8 @@ use crate::{audio::buffer::AudioBuffer, dsp::window::hann};
 const WINDOW_SIZE: usize = 1024;
 const ANALYSIS_HOP: usize = 256;
 
-/// Naive OLA time-stretch. Robotic artifacts expected (it does not sound human at all lol).
+/// Naive OLA time-stretch. Robotic artifacts expected (it does not sound human
+/// at all lol).
 pub fn time_stretch(input: &AudioBuffer, stretch: f32) -> AudioBuffer {
     let output_length = (input.samples.len() as f32 * stretch).ceil() as usize;
     let synthesis_hop = (ANALYSIS_HOP as f32 * stretch).floor() as usize;
@@ -27,6 +28,48 @@ pub fn time_stretch(input: &AudioBuffer, stretch: f32) -> AudioBuffer {
         for (i, out) in frame.iter().copied().enumerate() {
             if out_pos + i >= output.len() {
                 break;
+            }
+            output[out_pos + i] += out;
+        }
+
+        in_pos += ANALYSIS_HOP;
+        out_pos += synthesis_hop;
+    }
+
+    AudioBuffer {
+        sample_rate: input.sample_rate,
+        samples: output,
+    }
+}
+
+/// Time-stretch where it changes over time. The function takes in a value
+/// between 0 and 1 (where 0 is the first frame and 1 is the last) and outputs
+/// how much to stretch.
+pub fn time_glide(input: &AudioBuffer, mut stretch: impl FnMut(f32) -> f32) -> AudioBuffer {
+    let window = hann(WINDOW_SIZE);
+
+    let mut output = vec![0.0f32; input.samples.len()];
+
+    let mut in_pos = 0usize;
+    let mut out_pos = 0usize;
+
+    while (in_pos + WINDOW_SIZE) < input.samples.len() {
+        let t = in_pos as f32 / input.samples.len() as f32;
+
+        let stretch = stretch(t);
+        assert!(stretch > 0.0);
+
+        let synthesis_hop = (ANALYSIS_HOP as f32 * stretch) as usize;
+
+        let mut frame = input.samples[in_pos..(in_pos + WINDOW_SIZE)].to_vec();
+
+        for (i, offset) in window.iter().copied().enumerate() {
+            frame[i] *= offset;
+        }
+
+        for (i, out) in frame.iter().copied().enumerate() {
+            if out_pos + i >= input.samples.len() {
+                output.push(0.0f32);
             }
             output[out_pos + i] += out;
         }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ mod dsp;
 mod nice;
 mod phoneme;
 
-use std::error::Error;
+use std::{error::Error, f32::consts::TAU};
 
 use crate::{
     audio::wav,
-    dsp::{pitch::pitch_shift, stretch::time_stretch},
+    dsp::{pitch::pitch_shift, stretch::time_glide},
+    nice::lerp,
     phoneme::ipa::Vowel,
 };
 
@@ -18,15 +19,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         "samples/vowel_{}.wav",
         Vowel::OpenBackUnrounded.ipa()
     ))?;
-    println!("Original: {} samples ({} sample rate)", file.samples.len(), file.sample_rate);
+    println!(
+        "Original: {} samples ({} sample rate)",
+        file.samples.len(),
+        file.sample_rate
+    );
 
-    for ratio in [1.0f32, 0.5, 2.0] {
-        let buf = time_stretch(&pitch_shift(&file, ratio), ratio);
-        println!("{ratio}x pitch and time: {} samples ({} sample rate)", buf.samples.len(), buf.sample_rate);
-        wav::export_wav(
-            buf,
-            format!("output_{}.wav", ratio),
-        )?;
+    for ratio in [1.0f32, 0.5, 2.0, 0.25, 1.25] {
+        let buf = time_glide(&pitch_shift(&file, ratio), |t| 1.0 + 0.3 * (TAU * t).sin());
+        println!(
+            "{ratio}x pitch and time: {} samples ({} sample rate)",
+            buf.samples.len(),
+            buf.sample_rate
+        );
+        wav::export_wav(buf, format!("output_{}.wav", ratio))?;
     }
 
     Ok(())
